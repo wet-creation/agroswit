52152d7a73f98b810c242f66a54b1f90f8c6ea72
Pavlo Karas <thepavlokaras@gmail.com>
2024-04-18 10:50:02 +0300

    refactor: check response status

--- src/app/category/fetchCategories.ts
+++ src/app/category/fetchCategories.ts
@@ -3,8 +3,13 @@ import { env } from "@/env.mjs";
 export async function categoriesData(): Promise<Result<Category[], string>> {
   try {
     const res = await fetch(`${env.SERVER_URL}/api/v1/categories`);
-    const json = (await res.json()) as Category[];
-    return { ok: json, err: null };
+
+    if (res.status === 200) {
+      const json = (await res.json()) as Category[];
+      return { ok: json, err: null };
+    }
+
+    return { ok: null, err: await res.text() };
   } catch (e) {
     console.error(e);
     return { ok: null, err: "failed to fetch categories" };
@@ -15,7 +20,7 @@ export type Category = {
   id: number;
   name: string;
   description: null;
-  parentCategoryId: null; // use - where null
+  parentCategoryId: null; // use - when null
   properties: Property[];
   subcategories: Subcategory[];
 };
